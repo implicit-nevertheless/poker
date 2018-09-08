47068bb6f2d5c19fe0c1443a7c4fa6b1da5c5482
Douglas Denhartog <douglas@denhartog.com>
2018-09-08 11:26:38 -0700

    cards.py

---
+++ poker/cards.py
@@ -0,0 +1,103 @@
+class Card:
+    def __init__(
+        self,
+        suit,
+        value,
+    ):
+        self.suit = suit
+        self.value = value
+
+    def __repr__(
+        self,
+    ):
+        return '{}<{} of {}s>'.format(
+            self.__class__.__name__,
+            self.value.name,
+            self.suit.name,
+        )
+
+    def __eq__(
+        self,
+        other,
+    ):
+        return self.value == other.value
+
+    def __gt__(
+        self,
+        other,
+    ):
+        return self.value > other.value
+
+
+class CardSuit:
+    def __init__(
+        self,
+        name,
+    ):
+        self.name = name
+
+
+suits = [
+    CardSuit('Club'),
+    CardSuit('Diamond'),
+    CardSuit('Heart'),
+    CardSuit('Spade'),
+]
+
+
+class CardValue:
+    def __init__(
+        self,
+        value,
+        name,
+    ):
+        self.value = value
+        self.name = name
+
+    def __eq__(
+        self,
+        other,
+    ):
+        return self.value == other.value
+
+    def __gt__(
+        self,
+        other,
+    ):
+        return self.value > other.value
+
+
+card_names = [
+    'Two',
+    'Three',
+    'Four',
+    'Five',
+    'Six',
+    'Seven',
+    'Eight',
+    'Nine',
+    'Ten',
+    'Jack',
+    'Queen',
+    'King',
+    'Ace',
+]
+
+values = [
+    CardValue(
+        value,
+        name,
+    )
+    for value, name in enumerate(
+        card_names,
+        1,
+    )
+]
+deck = [
+    Card(
+        suit,
+        value,
+    )
+    for suit in suits
+    for value in values
+]
